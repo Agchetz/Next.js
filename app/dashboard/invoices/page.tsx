b4add8865e86a85fee99767778f02faf7cb7432c
chethan.annappa <cannappa.aspiresys@archkey.com>
2024-12-11 15:53:59 +0530

    pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -36,7 +36,7 @@ export default async function Invoices(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+      <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
